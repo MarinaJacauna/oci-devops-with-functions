d3dafa6112dcad51a45964aea7f790155fd4e39a
Mari <36379223+MarinaJacauna@users.noreply.github.com>
2022-05-28 22:00:40 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,9 +2,6 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
 variable "compartment_ocid" {}
 variable "region" {}
 
