859c18fa724f1dd3934129f0289d2dc61f158690
Mari <36379223+MarinaJacauna@users.noreply.github.com>
2022-05-28 22:45:39 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -25,16 +25,20 @@ terraform {
 }
 
 provider "oci" {
-            tenancy-ocid = var.tenancy-ocid
-            region       = var.region
-            }
-            provider "oci" {
-            alias        = "home-region"
-            tenancy-ocid = var.tenancy-ocid
-            region       = lookup(data.oci-identity-regions.home-region.regions[0], "name")
-            }
-            provider "oci" {
-            alias        = "current-region"
-            tenancy-ocid = var.tenancy-ocid
-            region       = var.region
-            }
+   tenancy_ocid = var.tenancy_ocid
+   region       = var.region
+  
+   }
+provider "oci" {
+ alias        = "home_region"
+ tenancy_ocid = var.tenancy_ocid
+ region       = lookup(data.oci_identity_regions.home_region.regions[0], "name")
+  
+  
+}
+ provider "oci" {
+ alias        = "current_region"
+ tenancy_ocid = var.tenancy_ocid
+ region       = var.region
+   
+ }
