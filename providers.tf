b59b4d16e6f796cda723e952dec2ab5840aaed0e
Mari <36379223+MarinaJacauna@users.noreply.github.com>
2022-05-28 21:41:19 -0300

    Update providers.tf
    update

--- providers.tf
+++ providers.tf
@@ -25,30 +25,16 @@ terraform {
 }
 
 provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  region       = var.region
-
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-}
-
-provider "oci" {
-  alias        = "home_region"
-  tenancy_ocid = var.tenancy_ocid
-  region       = lookup(data.oci_identity_regions.home_region.regions[0], "name")
-
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-}
-
-provider "oci" {
-  alias        = "current_region"
-  tenancy_ocid = var.tenancy_ocid
-  region       = var.region
-
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-}
+            tenancy-ocid = var.tenancy-ocid
+            region       = var.region
+            }
+            provider "oci" {
+            alias        = "home-region"
+            tenancy-ocid = var.tenancy-ocid
+            region       = lookup(data.oci-identity-regions.home-region.regions[0], "name")
+            }
+            provider "oci" {
+            alias        = "current-region"
+            tenancy-ocid = var.tenancy-ocid
+            region       = var.region
+            }
